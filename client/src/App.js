12e30322fd03c08317009c62a03ebc773836d725
Pankaj <meghanipankaj5@gmail.com>
2022-01-22 13:51:22 -0500

    Drawing frontend core done

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
-import Customers from './components/Customers';
+import DrawCanvas from './components/DrawCanvas'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-       <Customers></Customers>
-      </header>
+      <div className='App-header'>
+        <DrawCanvas />
+      </div>
     </div>
   );
 }
