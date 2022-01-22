20a3850a4b55717bace69914d808ace8f90ba6d3
Clayton Dunavant <clayton@claytondunavant.com>
2022-01-22 18:48:43 -0500

    connected question list to DB

--- client/src/App.js
+++ client/src/App.js
@@ -6,19 +6,22 @@ import { useEffect, useState } from 'react';
 
 function App() {
 
-  // const questionList = param.questions.map(question => (
-  //   <Prompts
-  //     id={question.id}
-  //     name={question.name}
-  //     key={question.id}
-  //   />
-  // ));
-
-  const [questions, modifyQuestions] = useState()
+  
+  const [questions, modifyQuestions] = useState([])
 
   useEffect(() => {
     fetch("/api/qurl").then(data => data.json()).then(data => modifyQuestions(data))
   }, [])
+
+  const questionList = questions.map(question => (
+     <Prompts
+       id={question._id}
+       key={question._id}
+       name={question.question}
+       url={question.url}
+     />
+   ));
+
 
 
   return (
@@ -33,7 +36,7 @@ function App() {
         className="question-list stack-large stack-exception"
         aria-labelledby="list-heading"
       >
-        { }
+      { questionList }
       </ul>
     </div>
   );
